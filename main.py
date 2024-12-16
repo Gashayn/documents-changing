e7e4f92d2d9c09079bffd2b0ee31aed2ec3c1226
Gashayn <iwantsomecheese01@mail.ru>
2024-12-16 20:33:22 +0300

    Устанавливаем межстрочный интервал и выравнивание по левому краю

--- main.py
+++ main.py
@@ -8,4 +8,9 @@ folder_path = 'case'
 for paragraph in doc.paragraphs:
         for run in paragraph.runs:
             run.font.name = 'Times New Roman'
-            run.font.size = Pt(14)-00
+            run.font.size = Pt(14)
+
+paragraph.paragraph_format.line_spacing = 1.5
+paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT  
+
+doc.save(doc_path)
