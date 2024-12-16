a0d8ee132ff25b178fe089e5d70f2912a3ab5e73
Alberd00 <albert.asanov.69@gmail.com>
2024-12-16 18:02:41 +0300

    Функция для изменения формта документа

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 from docx.shared import Inches
 
 folder_path = 'case'
+for paragraph in doc.paragraphs:
+        for run in paragraph.runs:
+            run.font.name = 'Times New Roman'
+            run.font.size = Pt(14)
